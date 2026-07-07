18fe910069af6af8d057de2938a5c26bb1f46676
vishumodidev <vishumodidev@gmail.com>
2026-07-07 21:23:17 +0530

    added inital commit

---
+++ hello.py
@@ -0,0 +1,48 @@
+print("Welcome to Propgramming")
+if True:
+    print("Helow")
+
+    age=20
+
+    if age >= 21:
+        print("Welcome")
+    else:
+        print("not eligible")
+
+   # This is comment
+    print("Hellow world")
+
+    """
+
+    This is 
+    multiline 
+    comment
+    """
+    print("multiline comment")
+
+    name="vishu"
+    age=25
+    salary=1000000
+
+
+    print(name )
+    print(age)
+    print(salary)
+
+    #  input name
+
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    salary = input("Enter your salary: ")
+
+    print(name)
+    print(age)
+    print(salary)
+    
+
+    # python formating
+
+    name="praveen"
+    print("Hellow ",name)
+    print("Hellow {}" .format(name))
+    print(f"hellow {name}")
